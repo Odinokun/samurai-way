2332e241979d8ce8c534b8c5f9aa39299a38fca2
odinokun <odinokun@gmail.com>
2023-09-24 20:06:14 +0200

    feat: lesson#31 - completed

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { createRef, FC } from 'react';
 
 import { DialogItem } from './DialogItem/DialogItem';
 import { Message } from './Message/Message';
@@ -14,15 +14,24 @@ export const Dialogs: FC<IProps> = ({ state }) => {
   const dialogsElements = state.dialogsData.map(dialog => (
     <DialogItem key={dialog.id} id={dialog.id} name={dialog.name} />
   ));
-
+  
   const messagesElements = state.messagesData.map(message => (
     <Message key={message.id} message={message.message} />
   ));
-
+  
+  const newMessageElement = createRef<HTMLTextAreaElement>();
+  const addMessage = () => console.log(newMessageElement.current?.value);
+  
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>{dialogsElements}</div>
-      <div className={s.messages}>{messagesElements}</div>
+      <div className={s.messages}>
+        {messagesElements}
+        <div>
+          <textarea ref={newMessageElement} placeholder="Enter your message"></textarea>
+          <button onClick={addMessage}>Add new message</button>
+        </div>
+      </div>
     </div>
   );
 };
